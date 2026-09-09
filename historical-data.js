3d42357c1ac0553c83346b952a96edc0d7ee06e6
MarrapodiStefano <stef976@gmail.com>
2026-09-09 21:04:03 +0200

    Aggiunge contenitore dati storici

---
+++ historical-data.js
@@ -0,0 +1,2 @@
+const HISTORICAL_DATA={version:'1.0.0',generatedAt:null,source:'Build automatico',seasonWeights:{'2023-2024':0.20,'2024-2025':0.30,'2025-2026':0.50},metrics:{goals:'gf / pv',assists:'ass / pv',mv:'stored',fm:'stored'},seasons:{'2023-2024':[],'2024-2025':[],'2025-2026':[]}};
+window.HISTORICAL_DATA=HISTORICAL_DATA;
