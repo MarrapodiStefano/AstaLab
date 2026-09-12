4636880ca982dcae4a0b0938a9d08ad52aae446b
MarrapodiStefano <stef976@gmail.com>
2026-09-12 21:42:07 +0200

    AstaLab 3.5.44: Oracolo rapido con animazione

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "asta-fantacalcio-v113";
+const CACHE = "asta-fantacalcio-v114";
 
 const ASSETS = [
     "./","./index.html","./app.js","./campetti.js","./ui-fixes.js","./oracolo.js","./brain-sync-fix.js","./brain-picker-fix.js","./players.js","./listone-version.json","./manifest.json","./icon.svg","./assets/campetto.JPG"
